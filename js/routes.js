9739c98f89ee81be1bc43e050fc10211e99d4afe
aladinputra <aldiansyahputra83@gmail.com>
2016-08-03 16:01:38 +0700

    comit aldian

--- js/routes.js
+++ js/routes.js
@@ -7,7 +7,16 @@ app.config(function($routeProvider){
     templateUrl : 'halaman/aboutview.html',
     controller : 'AboutController'
   })
+  .when('login',{
+    templateUrl : 'halaman/loginView.html'
+  })
 .otherwise({
   redirectTo : '/'
 })
+});
+
+$rootScope.$on('$locationChangeStart',function(event,next,current){
+  if($location.path() !== '/login' && !$rootScope.globals.currentUser){
+    $location.path('/login');
+  }
 });
